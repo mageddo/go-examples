30015dfb44c69c2eb4c043f3d712ad26f3140920
Elvis <edigitalb@gmail.com>
2016-06-12 18:42:39 -0300

    fixing import

--- gowiki-dao/webapp/controller/WikiViewController.go
+++ gowiki-dao/webapp/controller/WikiViewController.go
@@ -1,7 +1,9 @@
 package controller
 
-import "net/http"
-import "github.com/mageddo/go-examples/gowiki-dao/webapp/config"
+import (
+	"net/http"
+	"github.com/mageddo/go-examples/gowiki-dao/webapp/config"
+)
 
 func main(){
 	http.HandleFunc("/view/", config.MakeHandler(func (w http.ResponseWriter, r *http.Request, title string){
