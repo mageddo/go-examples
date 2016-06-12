3bfef32863b16b9714f8d5f1e6110469b414b8d4
Elvis <edigitalb@gmail.com>
2016-06-12 18:49:35 -0300

    fixing main

--- gowiki-dao/webapp/wiki.go
+++ gowiki-dao/webapp/wiki.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"html/template"
+	"github.com/mageddo/go-examples/gowiki-dao/webapp/config"
+	"github.com/mageddo/go-examples/gowiki-dao/webapp/dao/wiki"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-	"fmt"
 )
 
 func main() {
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/edit/", config.MakeHandler(editHandler))
+	http.HandleFunc("/save/", config.MakeHandler(saveHandler))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -19,21 +18,12 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+	p, err := wiki.LoadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	renderTemplate(w, "edit", p)
+	config.RenderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -45,39 +35,4 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = loadTemplates()
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func loadTemplates() *template.Template {
-	defer func(){
-		if str := recover(); str != nil {
-			str = fmt.Sprintf("Not found templates %s", str)
-			fmt.Println("error: ", str)
-			panic(str)
-		}
-	}()
-
-	var templates = template.Must(template.ParseFiles("view/edit.html", "view/view.html"))
-	return templates;
-}
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
 }
