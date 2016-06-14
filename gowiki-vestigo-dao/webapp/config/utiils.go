6ba9487a9c5dc0b661993fe891fbae526b66f18a
Elvis <edigitalb@gmail.com>
2016-06-13 22:35:03 -0300

    disabling old handler

--- gowiki-vestigo-dao/webapp/config/utiils.go
+++ gowiki-vestigo-dao/webapp/config/utiils.go
@@ -2,37 +2,10 @@ package config
 
 import (
 	"github.com/mageddo/go-examples/gowiki-dao/webapp/dao/wiki"
-	"github.com/mageddo/go-examples/gowiki-dao/webapp/req"
-	"regexp"
 	"net/http"
 	"fmt"
 	"html/template"
-	"bytes"
-	"log"
 )
-
-
-func MakeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-
-	var buffer bytes.Buffer
-	for e := req.Paths.Front(); e != nil; e = e.Next() {
-		buffer.WriteString(e.Value.(string))
-		buffer.WriteString("|")
-	}
-	buffer.Truncate(buffer.Len() - 1)
-	regex := fmt.Sprintf("^(%s)([a-zA-Z0-9]+)$", buffer.String())
-	log.Println("===> fixing: ", regex)
-	var validPath = regexp.MustCompile(regex)
-	//var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
 
 var templates = loadTemplates()
 
