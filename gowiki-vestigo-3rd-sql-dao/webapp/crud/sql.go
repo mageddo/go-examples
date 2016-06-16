7693e97d96304bf8043d79b9e912eab0cc04f023
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-06-16 14:30:05 -0300

    limiting idle conns

--- gowiki-vestigo-3rd-sql-dao/webapp/crud/sql.go
+++ gowiki-vestigo-3rd-sql-dao/webapp/crud/sql.go
@@ -13,7 +13,7 @@ var (
 	)
 func init(){
 	pool, err = sqlx.Open("postgres", "postgres://root:root@postgresql-server.dev/wiki?sslmode=disable")
-	pool.SetMaxIdleConns(5)
+	pool.SetMaxIdleConns(1)
 	pool.SetConnMaxLifetime(2 * time.Minute)
 	pool.SetMaxOpenConns(95)
 	if err != nil{
