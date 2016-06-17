f18a3ae2052e461eb651efdc1e58a4ee9b6a2862
Elvis <edigitalb@gmail.com>
2016-06-17 00:15:11 -0300

    removing dependency

--- filewritter/hello.go
+++ filewritter/hello.go
@@ -15,9 +15,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang/example/stringutil"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+	fmt.Println("!selpmaxe oG ,olleH")
 }
