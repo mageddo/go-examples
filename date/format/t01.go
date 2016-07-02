62fd33cf65737c261a1731f2b83ae9667ccf1494
Elvis <edigitalb@gmail.com>
2016-07-02 20:26:59 -0300

    adding others format layouts

--- date/format/t01.go
+++ date/format/t01.go
@@ -9,6 +9,9 @@ func main() {
 
 	t := time.Now()
 	fmt.Println(t.String())
-	fmt.Println(t.Format("2006-01-02 15:04:05"))
+	fmt.Println(t.Format("2006-01-02 15:04:05")) // work: year-month-day
+	fmt.Println(t.Format("2015-01-02 15:04:05")) // not work
+	fmt.Println(t.Format("02/01/2006 15:04:05")) // work day/month/year
+	fmt.Println(t.Format("02/01/2006")) // work day/month/year
 
 }
