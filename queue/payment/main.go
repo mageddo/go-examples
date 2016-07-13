1a083f9aa3993e6f7b313991084adc178fd712ba
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-07-13 19:23:44 -0300

    adding utilities

--- queue/payment/main.go
+++ queue/payment/main.go
@@ -13,6 +13,8 @@ type Payment struct {
 	status int8 // 0=not processed, 1=processing, 2=processed
 }
 
+var keep int = 1;
+
 func main() {
 
 	payments := &[]Payment{
@@ -22,16 +24,33 @@ func main() {
 	}
 
 	c := make(chan *Payment, 1)
+
+	// one guy reading from database and putting on the queue pool
 	go PaymentQueuePoolSender(c, payments)
 
-	for i:=1; i <= 10; i++ {
+	for i:=1; i <= 10; i++ { // 10 consumers of the queue
 		go PaymentQueueConsumer(c, i)
 	}
 
-	dataBasePopulator(payments)
+	// ever populating the database with new requests
+	go dataBasePopulator(payments)
 
-	var str string;
-	fmt.Scanln(&str)
+	// print the database stats time to times
+	go stats(payments)
+
+	// some queue useful commands
+	for cmd:="" ; ; fmt.Scanln(&cmd) {
+		switch cmd {
+		case "1":
+			keep = 1
+			log.Println("populate activated!")
+			break
+		case "0":
+			keep = 0
+			log.Println("populate disabled!")
+			break
+		}
+	}
 
 }
 
@@ -41,9 +60,12 @@ func main() {
 func dataBasePopulator(payments *[]Payment){
 	for i := 1; ; i++ {
 		time.Sleep(time.Millisecond * 100)
+		if keep != 1 {
+			continue
+		}
 		*payments = append((*payments), Payment{
 			debtor: fmt.Sprintf("debitor: %d", i),
-			creditor: fmt.Sprintf("debitor: %d", i),
+			creditor: fmt.Sprintf("credtor: %d", i),
 			value: 1.99,
 		})
 
@@ -78,5 +100,43 @@ func PaymentQueueConsumer(c <-chan *Payment, i int){
 		time.Sleep(time.Second * 10)
 		log.Printf("queue=pay-%d, payed!", i)
 		payment.status = 2;
+	}
+}
+
+
+/**
+ * All items on database that are not processed yet
+ */
+func countNotProcessedPayments(payments *[]Payment) int {
+	var count int = 0;
+	ps := (*payments)
+	for i:=0; i < len(ps) ; i++{
+		if ps[i].status == 0 {
+			count++;
+		}
+	}
+	return count
+}
+
+/**
+ * All items on database that are processing now
+ */
+func countProcessingPayments(payments *[]Payment) int {
+	var count int = 0;
+	ps := (*payments)
+	for i:=0; i < len(ps); i++{
+		if ps[i].status == 1 {
+			count++;
+		}
+	}
+	return count
+}
+
+func stats(payments *[]Payment){
+	for ;; {
+		log.Println("===========================")
+		log.Printf("not processed=%d, processing=%d\n", countNotProcessedPayments(payments), countProcessingPayments(payments))
+		log.Println("===========================")
+		time.Sleep(time.Second * 5)
 	}
 }
