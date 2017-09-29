016703a8ca99076f6c39f1e2259485e959ca9301
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-09-29 18:37:15 -0300

    adding context tests as unit tests

---
+++ building-microservices-with-go/context/test_test.go
@@ -0,0 +1,39 @@
+package context
+
+import (
+	"testing"
+	"net/http"
+	"time"
+	"fmt"
+	"context"
+)
+
+func TestSiteRequestCanceledByTimeut(t *testing.T){
+
+	r, _ := http.NewRequest("GET", "https://google.com", nil)
+
+	timeoutRequest, cancelFunc := context.WithTimeout(r.Context(), 1*time.Millisecond)
+	defer cancelFunc()
+
+	r = r.WithContext(timeoutRequest)
+
+	_, err := http.DefaultClient.Do(r)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func TestAnyMethodCallCanceledByTimeout(t *testing.T) {
+
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+	}
+}
