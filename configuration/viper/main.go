b0f6a43662bb72271ebb92abe8afff31d6b73b63
Elvis de Freitas <edigitalb@gmail.com>
2017-11-03 00:33:15 -0200

    adding configuration sample

--- configuration/viper/main.go
+++ configuration/viper/main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"github.com/spf13/viper"
+	"fmt"
+	"log"
+)
+
 func main(){
 
-	
+	viper.SetConfigName("application-prod")
+
+	// o path que ele achar primeiro, ele ja para, ou seja, nao mergia os diretorios
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+
+	log.Printf("name=%s", viper.Get("name"))
+	log.Printf("version=%s", viper.Get("version"))
+	log.Printf("user.name=%s", viper.Get("user.name"))
+	log.Printf("url=%s", viper.Get("url"))
+
 }
